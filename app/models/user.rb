4f9005d170603e7b07ef3e451fe8813a1a35c2ce
YouBent <541901525@qq.com>
2015-07-12 23:25:15 -0400

    email validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
                       length: { is: 10 }     
   validates :dob, format: { with: /((2-(0[1-9]|1[0-9]|2[0-8]))|((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30)))-(19|20)\d{2}/ } 
    
+  validates :email, format: {with: /(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})/}
 end
